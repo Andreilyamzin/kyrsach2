828fb45075a690ec05588c4742a1faa1861e7dd7
Lyamzin <lyamzin01@mail.ru>
2024-03-25 19:52:06 +1000

    d

---
+++ src/main/java/com/example/kyrsach2/Kyrsach2Application.java
@@ -0,0 +1,13 @@
+package com.example.kyrsach2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kyrsach2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kyrsach2Application.class, args);
+	}
+
+}
